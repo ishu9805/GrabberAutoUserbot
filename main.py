b6e354696e86998bf848bd1366b83f49f3e7444a
ishu9805 <abhishek47903@gmail.com>
2024-05-18 07:45:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ async def lootbot(bot, message):
     await bot.delete_messages(message.chat.id, sent_message.id)
 
 
-@app.on_message(filters.photo & filters.private & filters.user(grab_bot))
+@app.on_message(filters.photo & filters.group & filters.user(6501935889))
 async def grabbot(bot, message):
     # Reply to the bot with the desired response
     sent_message = await message.reply("/waifu@collect_waifu_cheats_bot")
